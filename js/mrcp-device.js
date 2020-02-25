c3b49722286c509e9ddb9cf5b7391614bae12e6d
Wright-Dj <wrightdj2000@gmail.com>
2020-02-25 08:29:57 -0800

    Add location

--- js/mrcp-device.js
+++ js/mrcp-device.js
@@ -51,6 +51,71 @@ class MRCPLock{
   }
 
 }
+
+class MRCPMeter{
+
+  constructor(){
+    this.rate = document.querySelector('#meter_rate');
+    this.duration = document.querySelector('#meter_duration');
+    this.connected = document.querySelector('#meter_connected');
+    console.log("done");
+    this.connected.style.display = "none";
+  }
+
+  async BLEconnect(){
+    var callback = (event)=>{
+      var status = document.querySelector('#meter_status');
+      var connected = document.querySelector('#meter_connected');
+      status.style.display = 'block';
+      connected.style.display = "block";
+      
+      let decoder = new TextDecoder("utf-8");
+      this.led_on = parseInt(decoder.decode(event.target.value)) == 1;
+      this.button.innerHTML = "Submit";
+      status.style.backgroundColor = this.led_on ? '#39cc39' : '#ff5757';
+  };
+    try {
+      console.log('Requesting Bluetooth Device...');
+      this.device  = await navigator.bluetooth.requestDevice({
+          filters: [{services: ['6e400001-b5a3-f393-e0a9-e50e24dcca9e']}]});
+          //acceptAllDevices: true, optionalServices: ['6e400001-b5a3-f393-e0a9-e50e24dcca9e']});
+    
+      console.log('Connecting to GATT Server...');
+      const server = await this.device.gatt.connect();
+    
+      console.log('Getting Service...');
+      const service = await server.getPrimaryService('6e400001-b5a3-f393-e0a9-e50e24dcca9e');
+    
+      console.log('Getting Characteristic TX...');
+      this.characteristic_tx = await service.getCharacteristic('6e400002-b5a3-f393-e0a9-e50e24dcca9e');
+
+      console.log('Getting Characteristic RX...');
+      this.characteristic_rx = await service.getCharacteristic('6e400003-b5a3-f393-e0a9-e50e24dcca9e');
+      this.characteristic_rx.addEventListener('characteristicvaluechanged', callback);
+      this.characteristic_rx.startNotifications();
+      console.log('Done');
+    } catch(error) {
+      console.log(error);
+    }
+    return Promise.resolve();
+  }
+
+
+  async buttonPress(button){
+    this.button = button;
+    if(this.device == null){
+      this.button.innerHTML = "Connecting";
+      await this.BLEconnect();
+    }else{
+      let encoder = new TextEncoder('utf-8');
+      let sendMsg = encoder.encode((this.led_on ? 'OFF' : 'ON').toString());
+      console.log('Writing Characteristic...');
+      this.button.innerHTML = 'Loading';
+      await this.characteristic_tx.writeValue(sendMsg);
+    }
+  }
+}
+
 class MRCPVendingMachine{
 
   async BLEconnect(){
@@ -101,5 +166,10 @@ class MRCPVendingMachine{
   }
 
 }
-mrcp_lock = new MRCPLock();
-mrcp_vending_machine = new MRCPVendingMachine();
+
+window.onload = ()=>{
+  mrcp_lock = new MRCPLock();
+  mrcp_meter = new MRCPMeter();
+  mrcp_vending_machine = new MRCPVendingMachine();
+};
+
